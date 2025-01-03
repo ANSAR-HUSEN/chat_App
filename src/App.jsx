ee26b7655a78ae4c781ac12d37bd580a76ca0dd2
ANSAR HUSEN <ansar.husen-ug@aau.edu.et>
2025-01-03 11:56:26 +0300

    auth ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
-import React from 'react'
-import { Button } from './components/components/ui/button'
+import React from "react";
+import { Button } from "./components/components/ui/button";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import Auth from "./pages/auth/Auth";
+import Chat from "./pages/chat/Chat";
+import Profile from "./pages/profile/Profile";
 
 function App() {
   return (
     <div>
-      <Button>Click me</Button>
+      {/* configure router */}
+      <BrowserRouter>
+        <Routes>
+          <Route path="/auth" element={<Auth />} />
+          <Route path="/chat" element={<Chat />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="*" element={<Auth />} />
+        </Routes>
+      </BrowserRouter>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
